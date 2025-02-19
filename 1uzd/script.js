76f2f27b012448871145b4a95a050acfb313f6b6
OlgaJusis <olgajusis@gmail.com>
2025-02-19 20:49:39 +0200

    Initial commit

---
+++ 1uzd/script.js
@@ -0,0 +1,13 @@
+console.log("pirma JavaScript uzduotis");
+let KasTai = "Pirma";
+console.log(KasTai);
+
+
+let kokiaUzd = 1;
+console.log(kokiaUzd);
+console.log(typeof kokiaUzd);
+
+
+let arPirma = true;
+console.log(arPirma);
+console.log(typeof arPirma);
