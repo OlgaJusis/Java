53f5c91904c5b88fab15c43aaf6bf241eabad057
OlgaJusis <olgajusis@gmail.com>
2025-02-19 21:19:19 +0200

    bandymas

---
+++ bandymas/script.js
@@ -0,0 +1,13 @@
+console.log("Olga is js failo");
+let bandymas = "Pirmas";
+console.log(bandymas);
+
+
+let koks = 1;
+console.log(koks);
+console.log(typeof koks);
+
+
+let arPirmas = true;
+console.log(arPirmas);
+console.log(typeof arPirmas);
