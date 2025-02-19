2fd9378f01d9182ec7b2826ef8673410afbed354
OlgaJusis <olgajusis@gmail.com>
2025-02-19 20:17:49 +0200

    Java pradzia

---
+++ script.js
@@ -0,0 +1,36 @@
+console.log("Olga is js failo");
+// type zodinis (String)
+let vardas = "Olga";
+
+console.log(vardas);
+// type number
+let amzius = 41;
+
+console.log(amzius);
+console.log(typeof amzius);
+
+// type boolean true/false
+let arSuauge = true;
+console.log(arSuauge);
+console.log(typeof arSuage);
+
+// type empty
+let tuscias = null;
+console.log(tuscias);
+console.log(typeof tuscias);
+
+// type indefined (neapibrezta/nepriskirta reiksme)
+let neapibreztas;
+console.log(neapibreztas);
+console.log(typeof neapibreztas);
+
+// referenciniai kintamuju tipai
+
+let masyvas = [1,2,3];
+console.log(masyvas);
+console.log(masyvas.length);
+console.log(masyvas[0]);
+console.log(masyvas[1]);
+console.log(masyvas[2]);
+masyvas.push("DENISAS");
+console.log(masyvas.length);
